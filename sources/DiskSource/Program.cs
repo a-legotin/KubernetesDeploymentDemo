837a572572f5fc6b93925f66fe54a5130bbd1d4c
Alexander Legotin <mrproz@yandex.ru>
2018-03-26 09:22:58 +0300

    added projects

---
+++ sources/DiskSource/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiskSource
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
