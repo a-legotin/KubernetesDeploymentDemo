94ed8bab23373d75132d4aad8dac3d9ef548020f
Alexander Legotin <mrproz@yandex.ru>
2018-06-07 22:25:09 +0300

    Added BUS classes

---
+++ common/Bus.Events/DocumentReceivedEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bus.Events
+{
+    class DocumentReceivedEvent : IntegrationEvent
+    {
+        public string DocumentSubject { get; set; }
+        public byte[] DocumentBody { get; set; }
+    }
+}
